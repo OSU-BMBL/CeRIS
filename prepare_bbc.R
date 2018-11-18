ceb762d94afba45c92f34d228a466de177883472
Wang Cankun <flykun0620@gmail.com>
2018-11-18 09:07:57 -0600

    Update BBC pipeline

--- prepare_bbc.R
+++ prepare_bbc.R
@@ -3,18 +3,17 @@
 
 library(stringr)
 library(tidyverse)
-library(seqinr)
 
 args <- commandArgs(TRUE)
-jobid <- args[1] # jobid
-#outFile <- args[2] # user job id
 
+srcDir <- args[1]
+setwd(srcDir)
+getwd()
 workdir <- getwd()
 alldir <- list.dirs(path = workdir)
 alldir <- grep("*_bic",alldir,value=T)
 
 
-filepath = get_dir_motif(alldir[1])[1]
 convert_motif <- function(filepath){
   this_line <- data.frame()
   motif_file <- file(filepath,"r")
@@ -30,6 +29,7 @@ j=1
   
 for (i in 1:length(alldir)) {
   combined_seq <- data.frame()
+  combined_gene <- data.frame()
   all_closure <- list.files(alldir[i],pattern = "*.closures",full.names = T)
   short_all_closure <- list.files(alldir[i],pattern = "*.closures",full.names = F)
   for (j in 1:length(all_closure)) {
@@ -40,8 +40,8 @@ for (i in 1:length(alldir)) {
     motif_seq <- motif_seq[,c(4,2)]
     colnames(motif_seq) <- c("info","seq")
     colnames(seq_file) <- c("info","genes")
-    write.table(seq_file,paste(alldir[i],".motifgene.txt",sep=""),sep = "\t" ,quote=F,row.names = F,col.names = T)
     combined_seq <- rbind(combined_seq,motif_seq)
+    combined_gene <- rbind(combined_gene,seq_file)
     res <- paste(alldir[i],".bbc.txt",sep="")
     #res <- file("filename", "w")
     cat("", file=res)
@@ -52,6 +52,8 @@ for (i in 1:length(alldir)) {
     }
   }
   cat(">end", file=res,sep="\n",append = T)
+  write.table(combined_gene,paste(alldir[i],".motifgene.txt",sep=""),sep = "\t" ,quote=F,row.names = F,col.names = T)
+  
 }
 
 
