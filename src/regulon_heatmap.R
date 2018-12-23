9f1373b6ee843e4e1a96e230f535f08d6c6fe01f
Wang Cankun <flykun0620@gmail.com>
2018-12-22 21:21:04 -0600

    Update regulon_heatmap.R

--- src/regulon_heatmap.R
+++ src/regulon_heatmap.R
@@ -1,76 +1,40 @@
 #######  Read all motif result, convert to input for BBC ##########
 # remove all empty files before this
 
-library(GenomicAlignments)
-library(ensembldb)
-library(EnsDb.Hsapiens.v86)
 library(magic)
 library(Matrix)
-library(ggplot2)
 library(reshape2)
 library(COMPASS)
 library(RColorBrewer)
 args <- commandArgs(TRUE)
-
+#setwd("D:/Users/flyku/Documents/IRIS3-data/test_regulon")
+#srcDir <- getwd()
+#jobid <-2018122223516 
 srcDir <- args[1]
+jobid <- args[2]
 setwd(srcDir)
 getwd()
 workdir <- getwd()
-all_regulon <- list.files(path = workdir,pattern = "._bic.regulon.txt$")
+all_regulon <- list.files(path = workdir,pattern = "._bic.regulon_gene_name.txt$")
 all_label <- list.files(path = workdir,pattern = ".+cell_label.txt$")[1]
 label_file <- read.table(all_label,header = T)
-exp_file <- read.table("2018111445745_raw_expression.txt",stringsAsFactors = F,header = T,check.names = F)
+exp_file <- read.table(paste(jobid,"_raw_expression.txt",sep = ""),stringsAsFactors = F,header = T,check.names = F)
 
 
 i=j=k=1
-multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
-  require(grid)
-  
-  # Make a list from the ... arguments and plotlist
-  plots <- c(list(...), plotlist)
-  
-  numPlots = length(plots)
-  
-  # If layout is NULL, then use 'cols' to determine layout
-  if (is.null(layout)) {
-    # Make the panel
-    # ncol: Number of columns of plots
-    # nrow: Number of rows needed, calculated from # of cols
-    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
-                     ncol = cols, nrow = ceiling(numPlots/cols))
-  }
-  
-  if (numPlots==1) {
-    print(plots[[1]])
-    
-  } else {
-    # Set up the page
-    grid.newpage()
-    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-    
-    # Make each plot, in the correct location
-    for (i in 1:numPlots) {
-      # Get the i,j matrix positions of the regions that contain this subplot
-      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-      
-      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
-                                      layout.pos.col = matchidx$col))
-    }
-  }
-}
-generate_name <- function(df){
-  genes <- regulon_file
-  for (i in 1:ncol(genes)) {
-    name <- colnames(genes)[i]
-    result <- genes(EnsDb.Hsapiens.v86, filter=list(GeneIdFilter(as.character(genes[,i]))), 
-                    return.type="data.frame", columns=c("gene_name"))
-  }
-}
+
+#generate_name <- function(df){
+#  genes <- regulon_file
+#  for (i in 1:ncol(genes)) {
+#    name <- colnames(genes)[i]
+#    result <- genes(EnsDb.Hsapiens.v86, filter=list(GeneIdFilter(as.character(genes[,i]))), 
+#                    return.type="data.frame", columns=c("gene_name"))
+#  }
+#}
 
 
 #result <- genes(EnsDb.Hsapiens.v86, filter=list(GeneIdFilter(rownames(exp_file))), return.type="data.frame", columns=c("gene_name"))
-result <- genes(EnsDb.Hsapiens.v86, filter=list(GeneNameFilter(rownames(exp_file)),GeneIdFilter("ENSG", "startsWith")), 
-                return.type="data.frame", columns=c("gene_id"))
+#result <- genes(EnsDb.Hsapiens.v86, filter=list(GeneNameFilter(rownames(exp_file)),GeneIdFilter("ENSG", "startsWith")), return.type="data.frame", columns=c("gene_id"))
 
 heat_matrix <- data.frame(matrix(ncol = nrow(label_file), nrow = 0))
 colnames(heat_matrix) <- as.character(label_file[,1])
@@ -86,8 +50,8 @@ for (i in 1:length(all_regulon)) {
   num_cell_type <- as.numeric(sub(".*_CT_ *(.*?) *_bic.*", "\\1", all_regulon[i]))
   regulon_cell_type <- label_file[which(label_file[,2] == num_cell_type),1]
   for (j in 1:nrow(regulon_file)) {
-    regulon_gene_name <- genes(EnsDb.Hsapiens.v86, filter=list(GeneIdFilter(as.character(regulon_file[,j]))), 
-                          return.type="data.frame", columns=c("gene_name"))[,1]
+    this_line_regulon <- as.character(unlist(regulon_file[j,]))
+    regulon_gene_name <- this_line_regulon[this_line_regulon!=""]
     for (k in 1:length(regulon_gene_name)) {
       index_gene_name[k] <- which(rownames(exp_file)==regulon_gene_name[k])
     }
@@ -101,14 +65,13 @@ for (i in 1:length(all_regulon)) {
     heat_matrix <- rbind(heat_matrix,tmp_heat_matrix)
     
   }
-
   final_list[[i]] <- as.matrix(heat_matrix)
-  
 }
 
 final_matrix <- do.call(adiag, final_list)
 final_matrix[final_matrix==0] <- NA
-write.table(final_matrix,"final_matrix.txt",quote = F)
+# block matrix
+#write.table(final_matrix,"final_matrix.txt",quote = F)
 heat_matrix <- data.frame(matrix(ncol = ncol(final_matrix), nrow = 0))
 colnames(heat_matrix) <- colnames(final_matrix)
 for (i in 1:length(all_regulon)) {
