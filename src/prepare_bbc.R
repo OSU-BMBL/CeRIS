fc866afa1db5d41913461fa41fc5177c2120df7c
Wang Cankun <flykun0620@gmail.com>
2018-12-28 05:07:34 -0600

    fix meme to bobro format, empty cells

--- src/prepare_bbc.R
+++ src/prepare_bbc.R
@@ -5,11 +5,14 @@ library(stringr)
 library(tidyverse)
 
 args <- commandArgs(TRUE)
-#setwd("D:/Users/flyku/Documents/IRIS3-data/test_regulon")
+#setwd("D:/Users/flyku/Documents/IRIS3-data/test_meme")
 #srcDir <- getwd()
-#jobid <-2018122223516 
+#jobid <-2018122833236 
+# is_meme <- 1
+# motif_len <- 12
 srcDir <- args[1]
 is_meme <- args[2] # no 0, yes 1
+motif_len <- args[3]
 setwd(srcDir)
 getwd()
 workdir <- getwd()
@@ -27,67 +30,93 @@ convert_motif <- function(filepath){
   close(motif_file)
   return(df)
 }
-filepath = all_closure[10]
+#i=38
+#filepath=all_closure[1]
 convert_meme <- function(filepath){
-  this_line <- data.frame()
-  motif_result <- character()
+  this_line <- matrix(0,ncol = 6)
+  this_line <- data.frame(this_line)
+  motif_result <- tibble()
+  line<-0
   motif_file <- file(filepath,"r")
   line = readLines(motif_file)
-  df <- line[substr(line,0,3) == "ENS"|substr(line,0,3) == "ens"]
-  for (i in 1:length(df)) {
-    this_line <- strsplit(df[i],"\\s+")[[1]]
-    if(length(grep(".+[ATCG]",this_line[5])) == 1){
-      motif_result <- rbind(motif_result,t(data.frame(this_line)))
-    }
-  }
   close(motif_file)
-  df_info = line[substr(line,0,5) == "MOTIF"]
-  all_motif_index <- 1
-  cat("", file=filepath)
-  #i=1
-  for (i in 1:length(df_info)) {
-    this_info <- strsplit(df_info[i],"\\s+")[[1]]
-    this_consensus <- this_info[2]
-    this_index <- i
-    this_motif_length <- this_info[6]
-    this_num_sites <- as.numeric(this_info[9])
-    this_pval <- this_info[15]
-    motif_idx_range <- seq(all_motif_index,all_motif_index + this_num_sites - 1)
-    all_motif_index <- all_motif_index + this_num_sites
-    this_motif_align <- motif_result[motif_idx_range,]
-    this_motif_align <- cbind(this_motif_align,paste(">Motif-",i,sep = ""))
-    this_motif_align[,4] <- as.numeric(this_motif_align[,2]) + as.numeric(this_motif_length) - 1
-    this_seq_idx <- sample(seq(1:this_num_sites))
-    this_motif_align[,6] <- this_seq_idx
-    this_motif_align <- this_motif_align[,c(7,6,2,4,5,3,1)]
-    cat("*********************************************************\n", file=filepath,append = T)
-    cat(paste(" Candidate Motif   ",this_index,sep=""), file=filepath,append = T)
-    cat("\n*********************************************************\n\n", file=filepath,append = T)
-    cat(paste(" Motif length: ",this_motif_length,"\n Motif number: ",this_num_sites,
-              "\n Motif Pvalue: ",this_pval,"\n\n",sep=""), file=filepath,append = T)
-    
-    cat(paste("\n------------------- Consensus sequences------------------\n",this_consensus,"\n\n",sep=""), file=filepath,append = T)
-    cat("------------------- Aligned Motif ------------------\n#Motif	Seq	start	end	Motif		Score	Info\n", file=filepath,append = T)
-    for (j in 1:nrow(this_motif_align)) {
-      cat(this_motif_align[j,], file=filepath,append = T,sep = "\t")
-      cat("\n", file=filepath,append = T)
+  if (nchar(line[1]) != 57) {
+    df <- line[substr(line,0,3) == "ENS"|substr(line,0,3) == "ens"]
+    for (i in 1:length(df)) {
+      this_line <- strsplit(df[i],"\\s+")[[1]]
+      
+      if(length(grep(".+[ATCG]",this_line[5])) == 1){
+        tmp_bind <- t(data.frame(this_line))
+        if(ncol(tmp_bind) < 6) {
+          if (nchar(as.character(tmp_bind[5])) < motif_len){
+            tmp_bind <- cbind(tmp_bind,"A")
+            tmp <- tmp_bind[4]
+            tmp_bind[4] <- tmp_bind[5]
+            tmp_bind[5] <- tmp
+          } else {
+            tmp_bind <- cbind(tmp_bind,"A")
+          }
+        }
+        motif_result <- rbind(motif_result,tmp_bind)
+      }
     }
-    cat("----------------------------------------------------\n\n", file=filepath,append = T)
+    
+    df_info = line[substr(line,0,5) == "MOTIF"]
+    all_motif_index <- 1
+    #filepath=paste(filepath,".test",sep = "")
+    cat("", file=filepath)
+    #i=1
+    for (i in 1:length(df_info)) {
+      this_info <- strsplit(df_info[i],"\\s+")[[1]]
+      this_consensus <- this_info[2]
+      this_index <- i
+      this_motif_length <- this_info[6]
+      this_num_sites <- as.numeric(this_info[9])
+      this_pval <- this_info[15]
+      motif_idx_range <- seq(all_motif_index,all_motif_index + this_num_sites - 1)
+      all_motif_index <- all_motif_index + this_num_sites
+      this_motif_align <- motif_result[motif_idx_range,]
+      this_motif_name <- paste(">Motif-",i,sep = "")
+      this_motif_align <- cbind(this_motif_align,this_motif_name)
+      this_motif_align[,4] <- as.numeric(as.character(this_motif_align[,2])) + as.numeric(this_motif_length) - 1
+      this_seq_idx <- sample(seq(1:this_num_sites))
+      this_motif_align[,6] <- this_seq_idx
+      colnames(this_motif_align) <- (c("V1","V2","V3","V4","V5","V6","V7"))
+      this_motif_align <- this_motif_align[,c(7,6,2,4,5,3,1)]
+      this_motif_align[, ] <- lapply(this_motif_align[, ], as.character)
+      cat("*********************************************************\n", file=filepath,append = T)
+      cat(paste(" Candidate Motif   ",this_index,sep=""), file=filepath,append = T)
+      cat("\n*********************************************************\n\n", file=filepath,append = T)
+      cat(paste(" Motif length: ",this_motif_length,"\n Motif number: ",this_num_sites,
+                "\n Motif Pvalue: ",this_pval,"\n\n",sep=""), file=filepath,append = T)
+      cat(paste("\n------------------- Consensus sequences------------------\n",this_consensus,"\n\n",sep=""), file=filepath,append = T)
+      cat("------------------- Aligned Motif ------------------\n#Motif	Seq	start	end	Motif		Score	Info\n", file=filepath,append = T)
+      for (j in 1:nrow(this_motif_align)) {
+        cat( as.character(this_motif_align[j, ]), file=filepath,append = T,sep = "\t")
+        cat("\n", file=filepath,append = T)
+      }
+      cat("----------------------------------------------------\n\n", file=filepath,append = T)
+    }
+    
+    
   }
+  
 }
 
-#i=1
-#j=1
+#i=3
+#j=44
 #info = "bic1.txt.fa.closures-1"  
 for (i in 1:length(alldir)) {
   combined_seq <- data.frame()
   combined_gene <- data.frame()
-  all_closure <- list.files(alldir[i],pattern = "*.closures",full.names = T)
-  short_all_closure <- list.files(alldir[i],pattern = "*.closures",full.names = F)
+  all_closure <- list.files(alldir[i],pattern = "*.closures$",full.names = T)
+  short_all_closure <- list.files(alldir[i],pattern = "*.closures$",full.names = F)
   for (j in 1:length(all_closure)) {
     if(is_meme == 1) {
       convert_meme(all_closure[j])
     }
+    #test
+    #motif_seq <- convert_motif(paste(all_closure[j],".test",sep = ""))[,c(1,5,7)]
     motif_seq <- convert_motif(all_closure[j])[,c(1,5,7)]
     motif_seq[,1] <- gsub(">Motif","",motif_seq[,1])
     motif_seq[,4] <- as.factor(paste(short_all_closure[j],motif_seq[,1],sep=""))
