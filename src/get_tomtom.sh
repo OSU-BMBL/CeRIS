ea1a75cc98c916d8c95726f0edf2195fb7dad4c6
Cankun.Wang <flykun0620@gmail.com>
2019-04-04 03:58:30 -0500

    remove duplicated jaspar db

--- src/get_tomtom.sh
+++ src/get_tomtom.sh
@@ -18,7 +18,8 @@ do
 	mkdir tomtom/$(basename "$file" .fsa.meme)/HOCOMOCO
 	mkdir tomtom/$(basename "$file" .fsa.meme)/JASPAR
 	nohup /home/www/html/iris3/program/meme/bin/tomtom  -no-ssc -oc tomtom/$(basename "$file" .fsa.meme)/HOCOMOCO -verbosity 1 -min-overlap 5 -mi 1 -dist pearson -evalue -thresh 10.0 $file /home/www/html/iris3/program/motif_databases/HUMAN/HOCOMOCOv11_full_HUMAN_mono_meme_format.meme /home/www/html/iris3/program/motif_databases/MOUSE/HOCOMOCOv11_full_MOUSE_mono_meme_format.meme &
-	nohup /home/www/html/iris3/program/meme/bin/tomtom  -no-ssc -oc tomtom/$(basename "$file" .fsa.meme)/JASPAR -verbosity 1 -min-overlap 5 -mi 1 -dist pearson -evalue -thresh 10.0 $file /home/www/html/iris3/program/motif_databases/JASPAR/JASPAR2018_CORE_non-redundant.meme /home/www/html/iris3/program/motif_databases/JASPAR/JASPAR2018_CORE_vertebrates_non-redundant.meme &
+	#nohup /home/www/html/iris3/program/meme/bin/tomtom  -no-ssc -oc tomtom/$(basename "$file" .fsa.meme)/JASPAR -verbosity 1 -min-overlap 5 -mi 1 -dist pearson -evalue -thresh 10.0 $file /home/www/html/iris3/program/motif_databases/JASPAR/JASPAR2018_CORE_non-redundant.meme /home/www/html/iris3/program/motif_databases/JASPAR/JASPAR2018_CORE_vertebrates_non-redundant.meme &
+	nohup /home/www/html/iris3/program/meme/bin/tomtom  -no-ssc -oc tomtom/$(basename "$file" .fsa.meme)/JASPAR -verbosity 1 -min-overlap 5 -mi 1 -dist pearson -evalue -thresh 10.0 $file /home/www/html/iris3/program/motif_databases/JASPAR/JASPAR2018_CORE_non-redundant.meme &
 
 	#echo "/home/www/html/iris3/program/meme/bin/tomtom  -no-ssc -oc tomtom/$(basename "$file" .fsa)/HOCOMOCO -verbosity 1 -min-overlap 5 -mi 1 -dist pearson -evalue -thresh 10.0 $file.meme /home/www/html/iris3/program/motif_databases/HUMAN/HOCOMOCOv11_full_HUMAN_mono_meme_format.meme /home/www/html/iris3/program/motif_databases/MOUSE/HOCOMOCOv11_full_MOUSE_mono_meme_format.meme "
 done
