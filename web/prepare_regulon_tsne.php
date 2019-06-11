f5ccd412e81b127947fec38da37879bf53cfbc24
Cankun Wang <flykun0620@gmail.com>
2019-06-11 11:38:57 -0400

    Update prepare_regulon_tsne.php

--- web/prepare_regulon_tsne.php
+++ web/prepare_regulon_tsne.php
@@ -1,9 +1,8 @@
 <?php
+	$id = $_GET['id'];
 	$jobid = $_GET['jobid'];
-	$regulon_id=$_GET['regulon_id'];
-	$species=$_GET['species'];
 	$wd = "/var/www/html/iris3/data/$jobid/";
-	system("cd $wd; nohup Rscript /var/www/html/iris3/program/get_similiar_regulon.R $wd $regulon_id $jobid &");
+	system("cd $wd; nohup Rscript /var/www/html/iris3/program/plot_regulon.R $wd $id &");
 	#echo "<table id='$table_content_id' border='1'>
 	#<tr>
 	#<th>$jobid</th>
@@ -11,20 +10,8 @@
 	#<th>$species </th>
 	#</tr></table>";
 	#$db_contents=file_get_contents($db_file);
-	$db_file= "$wd/similar_regulon/$regulon_id.similar.txt";
-	$fp = fopen("$db_file", 'r');
-	if ($fp){
-	while (($line = fgetcsv($fp, 0, "\t")) !== FALSE){
-		if ($line) {
-			$test_json['data'][] = array_map('trim',$line);
-			#print_r($count);
-		}
-	}
-	} else{
-		die("Unable to open file");
-	}
-	fclose($fp);
-	echo json_encode($test_json); 
+	$response_array['status'] = 'success';  
+	echo json_encode($response_array);
 	#print_r($json);
 	
 ?>
