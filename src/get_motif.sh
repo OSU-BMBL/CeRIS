2569010b9110fdf432c488c1753b960247047987
Cankun Wang <flykun0620@gmail.com>
2019-06-17 10:06:31 -0400

    Update get_motif.sh

--- src/get_motif.sh
+++ src/get_motif.sh
@@ -6,7 +6,7 @@ max_length=$3
 is_meme=$4
 files="$(find $dir -maxdepth 2 -name "*fa" -print)"
 echo "$files"
-while read -r species; do COMMAND; 
+while read -r species; 
 	for file in $files ;
 	do
 		while :; do
@@ -20,7 +20,6 @@ while read -r species; do COMMAND;
 			#/var/www/html/iris3/program/meme/bin/meme $file -p 8 -w $min_length -allw -mod zoops -nmotifs 10 -text -maxsites 100 -revcomp -dna -oc $file.closures > $file.closures &
 			/var/www/html/iris3/program/meme/bin/meme $file -nostatus -w 12 -allw -mod anr -revcomp -nmotifs 10 -objfun de -neg /var/www/html/iris3/program/bg_data/Human.bg.fa -dna -text > $file.meme.closures &
 			#/var/www/html/iris3/program/meme/bin/meme bic1.txt.fa -nostatus -w 12 -allw -mod anr -revcomp -nmotifs 10 -csites 1000 -objfun de -neg /var/www/html/iris3/program/bg_data/Human.bg.fa -dna -text > 
-			#http://meme-suite.org/info/status?service=MEME&id=appMEME_5.0.51560458259539-1344811313
 		else
 			/var/www/html/iris3/program/dminda/src/BoBro/BoBro -i $file -l $min_length -F -o 10 -Z /var/www/html/iris3/program/bg_data/$species.bg.fa&
 			
