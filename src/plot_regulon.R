31001bb3935b7dd2089d1be093f8ed600e75bcfc
Cankun Wang <flykun0620@gmail.com>
2019-06-27 17:22:04 -0400

    add custom methods plot

--- src/plot_regulon.R
+++ src/plot_regulon.R
@@ -15,10 +15,11 @@ args <- commandArgs(TRUE)
 #setwd("D:/Users/flyku/Documents/IRIS3-data/test_zscore")
 #setwd("C:/Users/wan268/Documents/iris3_data/0624")
 #srcDir <- getwd()
-#id <-"CT1S-R3" 
+#id <-"CT1S-R2" 
+#jobid <- "2019062485208"
 srcDir <- args[1]
 id <- args[2]
-
+jobid <- args[3]
 
 Plot.cluster2D<-function(reduction.method="tsne",module=1,customized=F,...){
   # my.plot.source<-GetReduceDim(reduction.method = reduction.method,module = module,customized = customized)
@@ -47,6 +48,7 @@ Plot.regulon2D<-function(reduction.method="tsne",regulon=1,cell.type=1,customize
                                     cell.type = cell.type,
                                     regulon = regulon,
                                     customized = customized)
+  print(my.plot.regulon)
   # if(!customized){
   #   my.plot.all.source<-cbind.data.frame(Embeddings(my.object,reduction = reduction.method),
   #                                        Cell_type=my.object$seurat_clusters)
@@ -60,7 +62,7 @@ Plot.regulon2D<-function(reduction.method="tsne",regulon=1,cell.type=1,customize
   # my.plot.source<-cbind.data.frame(my.plot.all.source,regulon.score=my.plot.regulon[my.plot.source.matchNumber,]$regulon.score)
   p.regulon<-ggplot(my.plot.regulon,
                     aes(x=my.plot.regulon[,1],y=my.plot.regulon[,2]))+xlab(colnames(my.plot.regulon)[1])+ylab(colnames(my.plot.regulon)[2])
-  p.regulon<-p.regulon+geom_point(aes(col=my.plot.regulon[,"regulon.score"]))+scale_color_gradient(low = "white",high = "red")
+  p.regulon<-p.regulon+geom_point(aes(col=my.plot.regulon[,"regulon.score"]))+scale_color_gradient(low = "grey",high = "red")
   #p.cluster<-theme_linedraw()
   p.regulon<-p.regulon + labs(col="regulon score")
   message("finish!")
@@ -98,20 +100,20 @@ Get.RegulonScore<-function(reduction.method="tsne",cell.type=1,regulon=1,customi
       my.cell.type<-my.cts.regulon.S4$Customized.idents
       message(c("using customized cell label: ","|", paste0(unique(as.character(my.cts.regulon.S4$Customized.idents)),"|")))
     }else{
-      my.cell.type<-my.cts.regulon.S4$seurat_clusters
+      my.cell.type<-as.numeric(my.cts.regulon.S4$seurat_clusters) 
       message(c("using default cell label(seurat prediction): ","|", paste0(unique(as.character(my.cts.regulon.S4$seurat_clusters)),"|")))
     } 
     tmp_data<-as.data.frame(my.cts.regulon.S4@assays$RNA@data)
     geneSets<-list(GeneSet1=rownames(tmp_data))
-    cells_AUC<-AUCell_calcAUC(geneSets,cells_rankings,aucMaxRank = nrow(cells_rankings)*0.5)
+    #cells_AUC<-AUCell_calcAUC(geneSets,cells_rankings,aucMaxRank = nrow(cells_rankings)*0.1)
     #cells_assignment<-AUCell_exploreThresholds(cells_AUC,plotHist = F,nCores = 1,assign = T)
-    my.auc.data<-as.data.frame(cells_AUC@assays@.xData$data$AUC)
-    my.auc.data<-t(my.auc.data[,colnames(tmp_data)])
-    # regulon.score<-colMeans(tmp_data)/apply(tmp_data,2,sd)
-    regulon.score<-my.auc.data
+    #my.auc.data<-as.data.frame(cells_AUC@assays@.xData$data$AUC)
+    #my.auc.data<-t(my.auc.data[,colnames(tmp_data)])
+    #regulon.score<-colMeans(tmp_data)/apply(tmp_data,2,sd)
+    regulon.score<-activity_score[regulon,]
     tmp.embedding<-Embeddings(my.object,reduction = reduction.method)[colnames(my.cts.regulon.S4),][,c(1,2)]
     my.choose.regulon<-cbind.data.frame(tmp.embedding,Cell_type=my.cell.type,
-                                        regulon.score=regulon.score[,1])
+                                        regulon.score=as.numeric(regulon.score))
     return(my.choose.regulon)
   }
 }
@@ -124,14 +126,13 @@ quiet <- function(x) {
 
 setwd(srcDir)
 my.object <- readRDS("seurat_obj.rds")
-cells_rankings<-quiet(AUCell_buildRankings(my.object@assays$RNA@data))
 
 regulon_ct <-gsub( "-.*$", "", id)
 regulon_ct <-gsub("[[:alpha:]]","",regulon_ct)
 regulon_id <- gsub( ".*R", "", id)
 regulon_id <- gsub("[[:alpha:]]","",regulon_id)
-#save.image(file="plot_regulon.RData")
 
+activity_score <- read.table(paste(jobid,"_CT_",regulon_ct,"_bic.activity_score.txt",sep = ""),row.names = 1,header = T)
 png(paste("regulon_id/overview_",id,".png",sep = ""),width=700, height=700)
 Plot.cluster2D(reduction.method = "tsne",customized = T)
 quiet(dev.off())
@@ -139,5 +140,11 @@ quiet(dev.off())
 png(paste("regulon_id/",id,".png",sep = ""),width=700, height=700)
 Plot.regulon2D(reduction.method = "tsne",regulon = as.numeric(regulon_id),cell.type=as.numeric(regulon_ct),customized =T)  
 quiet(dev.off())
+
+
+df <- as.data.frame(t(activity_score[21,]))
+df[,2] <- rownames(df)
+colnames(df)[1] <- "V1"
+ggplot(data=df,aes(x=V2,y=V1))+ geom_bar(stat = "identity")+ theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 
