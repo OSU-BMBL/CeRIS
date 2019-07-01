022dbfb7bd6c4f69782576bcd4148487712ea881
Cankun.Wang <flykun0620@gmail.com>
2019-06-30 21:54:33 -0400

    specify to python2.7

--- src/build_clustergrammar.sh
+++ src/build_clustergrammar.sh
@@ -16,10 +16,10 @@ do
 	done
 	out="$(basename $file .heatmap.txt)"
 	if [[ $out == *"S-R"* ]]; then
-	python /var/www/html/iris3/program/clustergrammer/make_clustergrammer.py $file $out $dir $jobid 0 &
+	python2.7 /var/www/html/iris3/program/clustergrammer/make_clustergrammer.py $file $out $dir $jobid 0 &
 	echo $file $out $dir $jobid $use_user_label
 	else
-    python /var/www/html/iris3/program/clustergrammer/make_clustergrammer.py $file $out $dir $jobid $use_user_label &
+    python2.7 /var/www/html/iris3/program/clustergrammer/make_clustergrammer.py $file $out $dir $jobid $use_user_label &
 	fi
 done
 wait
