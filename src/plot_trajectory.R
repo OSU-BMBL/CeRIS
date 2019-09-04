ac30586d3d4fbf02583c5e301dcac27d1cd15d6f
Cankun Wang <flykun0620@gmail.com>
2019-09-04 10:23:05 -0400

    Update: plot trajectory using UMAP

--- src/plot_trajectory.R
+++ src/plot_trajectory.R
@@ -156,12 +156,10 @@ Plot.Regulon.Trajectory<-function(customized=T,cell.type=1,regulon=1,start.clust
 
 Generate.Regulon<-function(cell.type=NULL,regulon=1,...){
   x<-Get.CellType(cell.type = cell.type)
-  my.rowname<-rownames(my.object)
-  gene.index<-sapply(x[[regulon]][-1],function(x) grep(paste0("^",x,"$"),ignore.case = T,my.rowname))
-  # my.object@data stores normalized data
-  tmp.regulon<-my.object@assays$RNA@data[gene.index,]
+  tmp.regulon<-subset(my.object,cells = colnames(my.object),features = x[[regulon]][-1])
   return(tmp.regulon)
 }
+
 Get.CellType<-function(cell.type=NULL,...){
   if(!is.null(cell.type)){
     my.cell.regulon.filelist<-list.files(pattern = "bic.regulon_gene_symbol.txt")
@@ -175,16 +173,33 @@ Get.CellType<-function(cell.type=NULL,...){
 
 
 
-Get.RegulonScore<-function(cell.type=1,regulon=1,...){
-  # recognize the regulon file pattern.
-  tmp.FileList<-list.files(pattern = "regulon_activity_score")
-  cell.type.index<-grep(paste0("_CT_",cell.type,"_bic"),tmp.FileList)
-  tmp.RegulonScore<-read.delim(tmp.FileList[cell.type.index],sep = "\t",check.names = F)[regulon,]
-  tmp.NameIndex<-match(rownames(my.object@reductions$tsne@cell.embeddings),names(tmp.RegulonScore))
-  tmp.RegulonScore<-tmp.RegulonScore[tmp.NameIndex]
-  tmp.RegulonScore.Numeric<- as.numeric(tmp.RegulonScore)
-  my.plot.regulon<-cbind.data.frame(my.object@reductions$tsne@cell.embeddings[,c(1,2)],regulon.score=tmp.RegulonScore.Numeric)
-  return(my.plot.regulon)
+Get.RegulonScore<-function(reduction.method="umap",cell.type=1,regulon=1,customized=F,...){
+  my.regulon.number<-length(Get.CellType(cell.type = cell.type))
+  if (regulon > my.regulon.number){
+    stop(paste0("Regulon number exceeds the boundary. Under this cell type, there are total ", my.regulon.number," regulons"))
+  } else {
+    my.cts.regulon.S4<-Generate.Regulon(cell.type = cell.type,regulon=regulon)
+    if(customized){
+      my.cell.type<-my.cts.regulon.S4$Customized.idents
+      message(c("using customized cell label: ","|", paste0(unique(as.character(my.cts.regulon.S4$Customized.idents)),"|")))
+    }else{
+      my.cell.type<-as.numeric(my.cts.regulon.S4$seurat_clusters) 
+      message(c("using default cell label(seurat prediction): ","|", paste0(unique(as.character(my.cts.regulon.S4$seurat_clusters)),"|")))
+    } 
+    tmp_data<-as.data.frame(my.cts.regulon.S4@assays$RNA@data)
+    geneSets<-list(GeneSet1=rownames(tmp_data))
+    #cells_AUC<-AUCell_calcAUC(geneSets,cells_rankings,aucMaxRank = nrow(cells_rankings)*0.1)
+    #cells_assignment<-AUCell_exploreThresholds(cells_AUC,plotHist = F,nCores = 1,assign = T)
+    #my.auc.data<-as.data.frame(cells_AUC@assays@.xData$data$AUC)
+    #my.auc.data<-t(my.auc.data[,colnames(tmp_data)])
+    #regulon.score<-colMeans(tmp_data)/apply(tmp_data,2,sd)
+    regulon.score<-t(as.matrix(activity_score[regulon,]))
+    tmp.embedding<-Embeddings(my.object,reduction = reduction.method)[colnames(my.cts.regulon.S4),][,c(1,2)]
+    my.choose.regulon<-cbind.data.frame(tmp.embedding,Cell_type=my.cell.type)
+    my.choose.regulon <- cbind(my.choose.regulon, regulon.score=regulon.score[match(rownames(my.choose.regulon), rownames(regulon.score))])
+    
+    return(my.choose.regulon)
+  }
 }
 quiet <- function(x) {
   sink(tempfile()) 
@@ -199,10 +214,9 @@ regulon_ct <-gsub("[[:alpha:]]","",regulon_ct)
 regulon_id <- gsub( ".*R", "", id)
 regulon_id <- gsub("[[:alpha:]]","",regulon_id)
 
-activity_score <- read.table(paste(jobid,"_CT_",regulon_ct,"_bic.regulon_activity_score.txt",sep = ""),row.names = 1,header = T,check.names = F)
 png(paste("regulon_id/overview_ct.trajectory.png",sep = ""),width=2000, height=1500,res = 300)
 if (!file.exists(paste("regulon_id/overview_ct.trajectory.png",sep = ""))){
-  #Plot.cluster2D(reduction.method = "tsne",customized = T)
+  #Plot.cluster2D(reduction.method = "umap",customized = T)
   #Plot.TrajectoryByCellType(customized = T)
   if(!exists("my.trajectory")){
     library(slingshot)
@@ -218,6 +232,8 @@ quiet(dev.off())
 
 png(paste("regulon_id/",id,".trajectory.png",sep = ""),width=2000, height=1500,res = 300)
 if (!file.exists(paste("regulon_id/",id,".trajectory.png",sep = ""))){
+  activity_score <- read.table(paste(jobid,"_CT_",regulon_ct,"_bic.regulon_activity_score.txt",sep = ""),row.names = 1,header = T,check.names = F)
+  
   if(!exists("my.trajectory")){
     library(slingshot)
     library(Seurat)
