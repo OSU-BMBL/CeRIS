8bbebbfd1d663011dc99cd7ef156e3b15ffce97a
Cankun Wang <flykun0620@gmail.com>
2019-09-05 10:16:51 -0400

    revert test

--- src/test_bootstrap_pvalue.R
+++ src/test_bootstrap_pvalue.R
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
    
+=======
+
+>>>>>>> parent of 818168f... test: rstudio server
 ########## Sort regulon by regulon specificity score ##################
 # used files:
 # filtered exp matrix
