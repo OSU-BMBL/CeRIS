70cfd04d3b6bc21a67bf749151e15c8a7dc9ad42
Cankun Wang <flykun0620@gmail.com>
2019-09-06 10:37:50 -0400

    Update: use TOMTOM default database

--- src/get_tomtom.sh
+++ src/get_tomtom.sh
@@ -15,13 +15,17 @@ do
     sleep 1
 	done
 	mkdir tomtom/$(basename "$file" .fsa.meme)
-	mkdir tomtom/$(basename "$file" .fsa.meme)/HOCOMOCO
-	mkdir tomtom/$(basename "$file" .fsa.meme)/JASPAR
-	nohup /var/www/html/iris3/program/meme/bin/tomtom  -no-ssc -oc tomtom/$(basename "$file" .fsa.meme)/HOCOMOCO -verbosity 1 -min-overlap 5 -mi 1 -dist pearson -evalue -thresh 10.0 $file /var/www/html/iris3/program/motif_databases/HUMAN/HOCOMOCOv11_full_HUMAN_mono_meme_format.meme /var/www/html/iris3/program/motif_databases/MOUSE/HOCOMOCOv11_full_MOUSE_mono_meme_format.meme &
+	#mkdir tomtom/$(basename "$file" .fsa.meme)/HOCOMOCO
+	#mkdir tomtom/$(basename "$file" .fsa.meme)/JASPAR
+	#hocomoco
+	#nohup /var/www/html/iris3/program/meme/bin/tomtom  -no-ssc -oc tomtom/$(basename "$file" .fsa.meme)/HOCOMOCO -verbosity 1 -min-overlap 5 -mi 1 -dist pearson -evalue -thresh 10.0 $file /var/www/html/iris3/program/motif_databases/HUMAN/HOCOMOCOv11_full_HUMAN_mono_meme_format.meme /var/www/html/iris3/program/motif_databases/MOUSE/HOCOMOCOv11_full_MOUSE_mono_meme_format.meme &
 	#nohup /var/www/html/iris3/program/meme/bin/tomtom  -no-ssc -oc tomtom/$(basename "$file" .fsa.meme)/JASPAR -verbosity 1 -min-overlap 5 -mi 1 -dist pearson -evalue -thresh 10.0 $file /var/www/html/iris3/program/motif_databases/JASPAR/JASPAR2018_CORE_non-redundant.meme /var/www/html/iris3/program/motif_databases/JASPAR/JASPAR2018_CORE_vertebrates_non-redundant.meme &
-	nohup /var/www/html/iris3/program/meme/bin/tomtom  -no-ssc -oc tomtom/$(basename "$file" .fsa.meme)/JASPAR -verbosity 1 -min-overlap 5 -mi 1 -dist pearson -evalue -thresh 10.0 $file /var/www/html/iris3/program/motif_databases/JASPAR/JASPAR2018_CORE_non-redundant.meme &
+	# jaspar
+	#nohup /var/www/html/iris3/program/meme/bin/tomtom  -no-ssc -oc tomtom/$(basename "$file" .fsa.meme)/JASPAR -verbosity 1 -min-overlap 5 -mi 1 -dist pearson -evalue -thresh 10.0 $file /var/www/html/iris3/program/motif_databases/JASPAR/JASPAR2018_CORE_non-redundant.meme &
 
 	#echo "/var/www/html/iris3/program/meme/bin/tomtom  -no-ssc -oc tomtom/$(basename "$file" .fsa)/HOCOMOCO -verbosity 1 -min-overlap 5 -mi 1 -dist pearson -evalue -thresh 10.0 $file.meme /var/www/html/iris3/program/motif_databases/HUMAN/HOCOMOCOv11_full_HUMAN_mono_meme_format.meme /var/www/html/iris3/program/motif_databases/MOUSE/HOCOMOCOv11_full_MOUSE_mono_meme_format.meme "
+	#default from tomtom website
+	nohup /var/www/html/iris3/program/meme/bin/tomtom  -no-ssc -oc tomtom/$(basename "$file" .fsa.meme)/ -verbosity 1 -min-overlap 5 -mi 1 -dist pearson -evalue -thresh 10.0 $file /var/www/html/iris3/program/motif_databases/EUKARYOTE/jolma2013.meme /var/www/html/iris3/program/motif_databases/JASPAR/JASPAR2018_CORE_vertebrates_non-redundant.meme /var/www/html/iris3/program/motif_databases/MOUSE/uniprobe_mouse.meme &
 done
 wait
 
