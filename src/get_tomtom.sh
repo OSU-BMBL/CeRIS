f1456353062235f008e5e1da6faafde24a170253
Cankun Wang <flykun0620@gmail.com>
2019-09-10 09:08:24 -0400

    Update: Use HOCOMOCO database

--- src/get_tomtom.sh
+++ src/get_tomtom.sh
@@ -2,7 +2,7 @@
 cores=8
 dir=$1
 mkdir tomtom
-
+species=$(head -n 1 species_main.txt)
 files="$(find $dir -maxdepth 2 -name "*.meme" -print)"
 echo "$files"
 for file in $files ;
@@ -25,7 +25,15 @@ do
 
 	#echo "/var/www/html/iris3/program/meme/bin/tomtom  -no-ssc -oc tomtom/$(basename "$file" .fsa)/HOCOMOCO -verbosity 1 -min-overlap 5 -mi 1 -dist pearson -evalue -thresh 10.0 $file.meme /var/www/html/iris3/program/motif_databases/HUMAN/HOCOMOCOv11_full_HUMAN_mono_meme_format.meme /var/www/html/iris3/program/motif_databases/MOUSE/HOCOMOCOv11_full_MOUSE_mono_meme_format.meme "
 	#default from tomtom website
-	nohup /var/www/html/iris3/program/meme/bin/tomtom  -no-ssc -oc tomtom/$(basename "$file" .fsa.meme)/ -verbosity 1 -min-overlap 5 -mi 1 -dist pearson -evalue -thresh 10.0 $file /var/www/html/iris3/program/motif_databases/EUKARYOTE/jolma2013.meme /var/www/html/iris3/program/motif_databases/JASPAR/JASPAR2018_CORE_vertebrates_non-redundant.meme /var/www/html/iris3/program/motif_databases/MOUSE/uniprobe_mouse.meme &
+	#nohup /var/www/html/iris3/program/meme/bin/tomtom  -no-ssc -oc tomtom/$(basename "$file" .fsa.meme)/ -verbosity 1 -min-overlap 5 -mi 1 -dist pearson -evalue -thresh 10.0 $file /var/www/html/iris3/program/motif_databases/EUKARYOTE/jolma2013.meme /var/www/html/iris3/program/motif_databases/JASPAR/JASPAR2018_CORE_vertebrates_non-redundant.meme /var/www/html/iris3/program/motif_databases/MOUSE/uniprobe_mouse.meme &
+	#TOMTOM human&mouse 
+	#nohup /var/www/html/iris3/program/meme/bin/tomtom  -no-ssc -oc tomtom/$(basename "$file" .fsa.meme)/ -verbosity 1 -min-overlap 5 -mi 1 -dist pearson -evalue -thresh 10.0 $file /var/www/html/iris3/program/motif_databases/HUMAN/HOCOMOCOv11_full_HUMAN_mono_meme_format.meme /var/www/html/iris3/program/motif_databases/MOUSE/HOCOMOCOv11_full_MOUSE_mono_meme_format.meme &
+	if [ "$species" == "Human" ]; then
+	nohup /var/www/html/iris3/program/meme/bin/tomtom  -no-ssc -oc tomtom/$(basename "$file" .fsa.meme)/ -verbosity 1 -min-overlap 5 -mi 1 -dist pearson -evalue -thresh 10.0 $file /var/www/html/iris3/program/motif_databases/HUMAN/HOCOMOCOv11_full_HUMAN_mono_meme_format.meme &
+	fi
+	if [ "$species" == "Mouse" ]; then
+	nohup /var/www/html/iris3/program/meme/bin/tomtom  -no-ssc -oc tomtom/$(basename "$file" .fsa.meme)/ -verbosity 1 -min-overlap 5 -mi 1 -dist pearson -evalue -thresh 10.0 $file /var/www/html/iris3/program/motif_databases/MOUSE/HOCOMOCOv11_full_MOUSE_mono_meme_format.meme &
+fi
 done
 wait
 
