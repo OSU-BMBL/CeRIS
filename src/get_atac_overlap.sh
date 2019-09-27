6ffb50a7eb24132af50c7671bd453db6d7c3657a
Cankun <flykun0620@gmail.com>
2019-09-27 11:30:27 -0400

    Fix: species detection

--- src/get_atac_overlap.sh
+++ src/get_atac_overlap.sh
@@ -2,11 +2,13 @@
 
 # input format: 1,1,1
 dir=$1
-atac_dir=/var/www/html/iris3/program/db/human_atac_seq
 mkdir atac
+
+while read -r species; do 
+atac_dir=/var/www/html/iris3/program/db/${species}_atac_seq
 files="$(find $dir -maxdepth 2 -name "*.regulon_motif.txt" -print)"
 bed_files="$(find $atac_dir -maxdepth 2 -name "*.bed" )"
-for file in $files
+for file in $files;
 do
 	while IFS=$'\t' read -r -a myArray
 	do
@@ -15,4 +17,4 @@ do
 		awk -v str="$regex" -v ct="$celltype" '$6 ~ str && $5 ~ ct' motif_position.bed > atac/$myArray.bed
 	done < $file
 done
-
+done < $dir/species.txt
