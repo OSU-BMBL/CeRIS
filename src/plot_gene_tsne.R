321e7b00d3c7f6bef4e04ca24d8b3487eef0524f
Cankun <flykun0620@gmail.com>
2019-12-17 13:15:37 -0500

    Fix: save pdf plot

--- src/plot_gene_tsne.R
+++ src/plot_gene_tsne.R
@@ -79,55 +79,6 @@ Plot.cluster2D<-function(reduction.method="umap",module=1,customized=T,pt_size=1
 }
 
 
-
-
-Generate.Regulon<-function(cell.type=NULL,regulon=1,...){
-  x<-Get.CellType(cell.type = cell.type)
-  tmp.regulon<-subset(my.object,cells = colnames(my.object),features = x[[regulon]][-1])
-  return(tmp.regulon)
-}
-
-Get.CellType<-function(cell.type=NULL,...){
-  if(!is.null(cell.type)){
-    my.cell.regulon.filelist<-list.files(pattern = "bic.regulon_gene_symbol.txt")
-    my.cell.regulon.indicator<-grep(paste0("_",as.character(cell.type),"_bic"),my.cell.regulon.filelist)
-    my.cts.regulon.raw<-readLines(my.cell.regulon.filelist[my.cell.regulon.indicator])
-    my.regulon.list<-strsplit(my.cts.regulon.raw,"\t")
-    return(my.regulon.list)
-  }else{stop("please input a cell type")}
-  
-}
-
-
-
-Get.RegulonScore<-function(reduction.method="tsne",cell.type=1,regulon=1,customized=F,...){
-  my.regulon.number<-length(Get.CellType(cell.type = cell.type))
-  if (regulon > my.regulon.number){
-    stop(paste0("Regulon number exceeds the boundary. Under this cell type, there are total ", my.regulon.number," regulons"))
-  } else {
-    my.cts.regulon.S4<-Generate.Regulon(cell.type = cell.type,regulon=regulon)
-    if(customized){
-      my.cell.type<-my.cts.regulon.S4$Customized.idents
-      message(c("using customized cell label: ","|", paste0(unique(as.character(my.cts.regulon.S4$Customized.idents)),"|")))
-    }else{
-      my.cell.type<-as.numeric(my.cts.regulon.S4$seurat_clusters) 
-      message(c("using default cell label(seurat prediction): ","|", paste0(unique(as.character(my.cts.regulon.S4$seurat_clusters)),"|")))
-    } 
-    tmp_data<-as.data.frame(my.cts.regulon.S4@assays$RNA@data)
-    geneSets<-list(GeneSet1=rownames(tmp_data))
-    #cells_AUC<-AUCell_calcAUC(geneSets,cells_rankings,aucMaxRank = nrow(cells_rankings)*0.1)
-    #cells_assignment<-AUCell_exploreThresholds(cells_AUC,plotHist = F,nCores = 1,assign = T)
-    #my.auc.data<-as.data.frame(cells_AUC@assays@.xData$data$AUC)
-    #my.auc.data<-t(my.auc.data[,colnames(tmp_data)])
-    #regulon.score<-colMeans(tmp_data)/apply(tmp_data,2,sd)
-    regulon.score<-t(as.matrix(activity_score[regulon,]))
-    tmp.embedding<-Embeddings(my.object,reduction = reduction.method)[colnames(my.cts.regulon.S4),][,c(1,2)]
-    my.choose.regulon<-cbind.data.frame(tmp.embedding,Cell_type=my.cell.type)
-    my.choose.regulon <- cbind(my.choose.regulon, regulon.score=regulon.score[match(rownames(my.choose.regulon), rownames(regulon.score))])
-    
-    return(my.choose.regulon)
-  }
-}
 quiet <- function(x) {
   sink(tempfile()) 
   on.exit(sink()) 
@@ -158,14 +109,3 @@ if (!file.exists(paste("regulon_id/",gene_symbol,".umap.pdf",sep = ""))){
   quiet(dev.off())
   
 }
-#png(paste("regulon_id/overview_ct.png",sep = ""),width=2000, height=1500,res = 300)
-#if (!file.exists(paste("regulon_id/overview_ct.png",sep = ""))){
-#  if(!exists("my.object")){
-#    library(Seurat)
-#    my.object <- readRDS("seurat_obj.rds")
-#  }
-#  num_cells <- ncol(my.object)
-#  pt_size <- get_point_size(num_cells)*2
-#  Plot.cluster2D(reduction.method = "umap",customized = T,pt_size = pt_size)
-#}
-#quiet(dev.off())
